e52f337151608413ab45a96fd1c56fb35ec0281f
Gabriel <gabrielferreiradsl@hotmail.com>
2022-03-18 16:54:25 -0300

    route "/matchs" integration tests added, TDD

---
+++ app/backend/src/tests/mocks/matchsMock.ts
@@ -0,0 +1,32 @@
+export default {
+    matchsWithoutFilter: [
+        {
+          id: 1,
+          homeTeam: 16,
+          homeTeamGoals: 1,
+          awayTeam: 8,
+          awayTeamGoals: 1,
+          inProgress: false,
+          homeClub: {
+            clubName: 'São Paulo'
+          },
+          awayClub: {
+            clubName: 'Grêmio'
+          }
+        },
+        {
+          id: 41,
+          homeTeam: 16,
+          homeTeamGoals: 2,
+          awayTeam: 9,
+          awayTeamGoals: 0,
+          inProgress: true,
+          homeClub: {
+            clubName: 'São Paulo'
+          },
+          awayClub: {
+            clubName: 'Internacional'
+          }
+        },
+      ],
+}

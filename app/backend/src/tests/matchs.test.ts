8fff5a18665b4a1f84a30ee6684f0d1688dbb24b
Gabriel <gabrielferreiradsl@hotmail.com>
2022-03-18 17:54:28 -0300

    route "/matchs/inProgress" integration tests added, TDD

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -17,6 +17,7 @@ const { expect } = chai;
 
 describe('matchs Route', () => {
     const ENDPOINT_MATCHS = '/matchs';
+    const ENDPOINT_MATCHS_INPROGRESS = '/matchs/inProgress';
     
     describe('"/matchs" Route', () => {
 
@@ -49,6 +50,79 @@ describe('matchs Route', () => {
             it('Deve retornar todos os matchs', () => {
                 expect(request.body).to.have.length(2);
                 expect(request.body).to.deep.eq(matchsMock.matchsWithoutFilter);
+            });
+        });
+    });
+
+    describe('"/matchs/inProgress" Route', () => {
+
+        describe('Quando a requisição é feita com o filtro "true"', async() => {
+            let request: Response;
+            beforeEach(async() => {
+            sinon.stub(Matchs, "findAll").resolves(matchsMock.matchsWithProgressTrue as any);
+            request = await chai.request(app).get(`${ENDPOINT_MATCHS_INPROGRESS}=true`);
+            });
+            
+            afterEach(() => {
+                sinon.restore();
+            });
+
+            it('Deve retornar o status: 200', async() => {
+                expect(request).to.have.status(200);
+            });
+            it('Deve retornar todos os campos', () => {
+                expect(request.body[0]).to.have.property('id');
+                expect(request.body[0]).to.have.property('homeTeam');
+                expect(request.body[0]).to.have.property('homeTeamGoals');
+                expect(request.body[0]).to.have.property('awayTeam');
+                expect(request.body[0]).to.have.property('awayTeamGoals');
+                expect(request.body[0]).to.have.property('inProgress');
+                expect(request.body[0]).to.have.property('homeClub');
+                expect(request.body[0]).to.have.property('awayClub');
+                expect(request.body[0].awayClub).to.have.property('clubName');
+                expect(request.body[0].homeClub).to.have.property('clubName');
+            });
+            it('Deve retornar todos os matchs', () => {
+                expect(request.body).to.have.length(matchsMock.matchsWithProgressTrue.length);
+                expect(request.body).to.deep.eq(matchsMock.matchsWithProgressTrue);
+            });
+            it('Deve retornar todos os matchs com o inProgress: "true"', () => {
+                expect(request.body.every(({ inProgress }) => inProgress === true )).to.be.eq(true);
+            });
+        });
+
+        describe('Quando a requisição é feita com o filtro "false"', async() => {
+            let request: Response;
+            beforeEach(async() => {
+            sinon.stub(Matchs, "findAll").resolves(matchsMock.matchsWithProgressFalse as any);
+            request = await chai.request(app).get(`${ENDPOINT_MATCHS_INPROGRESS}=false`);
+            });
+            
+            afterEach(() => {
+                sinon.restore();
+            });
+
+            it('Deve retornar o status: 200', async() => {
+                expect(request).to.have.status(200);
+            });
+            it('Deve retornar todos os campos', () => {
+                expect(request.body[0]).to.have.property('id');
+                expect(request.body[0]).to.have.property('homeTeam');
+                expect(request.body[0]).to.have.property('homeTeamGoals');
+                expect(request.body[0]).to.have.property('awayTeam');
+                expect(request.body[0]).to.have.property('awayTeamGoals');
+                expect(request.body[0]).to.have.property('inProgress');
+                expect(request.body[0]).to.have.property('homeClub');
+                expect(request.body[0]).to.have.property('awayClub');
+                expect(request.body[0].awayClub).to.have.property('clubName');
+                expect(request.body[0].homeClub).to.have.property('clubName');
+            });
+            it('Deve retornar todos os matchs', () => {
+                expect(request.body).to.have.length(matchsMock.matchsWithProgressFalse.length);
+                expect(request.body).to.deep.eq(matchsMock.matchsWithProgressFalse);
+            });
+            it('Deve retornar todos os matchs com o inProgress: "false"', () => {
+                expect(request.body.every(({ inProgress }) => inProgress === true )).to.be.eq(false);
             });
         });
     });
--- app/backend/src/tests/mocks/matchsMock.ts
+++ app/backend/src/tests/mocks/matchsMock.ts
@@ -29,4 +29,64 @@ export default {
           }
         },
       ],
+      matchsWithProgressTrue: [
+        {
+          "id": 41,
+          "homeTeam": 16,
+          "homeTeamGoals": 2,
+          "awayTeam": 9,
+          "awayTeamGoals": 0,
+          "inProgress": true,
+          "homeClub": {
+            "clubName": "São Paulo"
+          },
+          "awayClub": {
+            "clubName": "Internacional"
+          }
+        },
+        {
+          "id": 42,
+          "homeTeam": 6,
+          "homeTeamGoals": 1,
+          "awayTeam": 1,
+          "awayTeamGoals": 0,
+          "inProgress": true,
+          "homeClub": {
+            "clubName": "Ferroviária"
+          },
+          "awayClub": {
+            "clubName": "Avaí/Kindermann"
+          }
+        }
+      ],
+      matchsWithProgressFalse: [
+        {
+          "id": 1,
+          "homeTeam": 16,
+          "homeTeamGoals": 1,
+          "awayTeam": 8,
+          "awayTeamGoals": 1,
+          "inProgress": false,
+          "homeClub": {
+            "clubName": "São Paulo"
+          },
+          "awayClub": {
+            "clubName": "Grêmio"
+          }
+        },
+        {
+          "id": 2,
+          "homeTeam": 9,
+          "homeTeamGoals": 1,
+          "awayTeam": 14,
+          "awayTeamGoals": 1,
+          "inProgress": false,
+          "homeClub": {
+            "clubName": "Internacional"
+          },
+          "awayClub": {
+            "clubName": "Santos"
+          }
+        }
+      ],
 }

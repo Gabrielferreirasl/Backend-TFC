9d3bda6f2ab0afc8dc9df99d89d8bd7aec77b9c7
Gabriel <gabrielferreiradsl@hotmail.com>
2022-03-20 12:45:58 -0300

    "/leaderboard/away", integration tests

--- app/backend/src/tests/leaderboards.test.ts
+++ app/backend/src/tests/leaderboards.test.ts
@@ -53,4 +53,40 @@ describe('Leaderboard Route', () => {
             });
         });
     });
+
+    describe('"/leaderboard/away" Route', () => {
+
+        describe('Quando a requisição é feita com o filtro "away"', async() => {
+            let request: Response;
+            beforeEach(async() => {
+            sinon.stub(Matchs, "findAll").resolves(matchsMock.allMatchs as any);
+            sinon.stub(Clubs, "findAll").resolves(clubsMock.allclubs as any);
+            request = await chai.request(app).get(`${ENDPOINT_LEADERBOARD}/away`);
+            });
+            
+            afterEach(() => {
+                sinon.restore();
+            });
+
+            it('Deve retornar o status: 200', async() => {
+                expect(request).to.have.status(200);
+            });
+            it('Deve retornar todos os campos', () => {
+                expect(request.body[0]).to.have.property('name');
+                expect(request.body[0]).to.have.property('totalPoints');
+                expect(request.body[0]).to.have.property('totalGames');
+                expect(request.body[0]).to.have.property('totalVictories');
+                expect(request.body[0]).to.have.property('totalDraws');
+                expect(request.body[0]).to.have.property('totalLosses');
+                expect(request.body[0]).to.have.property('goalsFavor');
+                expect(request.body[0]).to.have.property('goalsOwn');
+                expect(request.body[0]).to.have.property('goalsBalance');
+                expect(request.body[0]).to.have.property('efficiency');
+            });
+            it('Deve retornar a tabela filtrada por Home', () => {
+                expect(request.body).to.have.length(clubsMock.allclubs.length);
+                expect(request.body).to.deep.eq(leaderboardsMock.leaderboardFilterAway);
+            });
+        });
+    });
 });

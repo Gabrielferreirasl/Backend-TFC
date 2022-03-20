e2698cb1a1a306f64cc6b34410c539f11195ff5b
Gabriel <gabrielferreiradsl@hotmail.com>
2022-03-20 13:18:47 -0300

    "/leaderboard" route, controllers and services

--- app/backend/src/database/routes/index.ts
+++ app/backend/src/database/routes/index.ts
@@ -7,17 +7,19 @@ import UsersValidations from '../middlewares/usersValidations';
 
 const route = express.Router();
 
-route.patch('/matchs/:id/finish', MatchsControllers.finishMatch);
-route.patch('/matchs/:id', MatchsControllers.updateMatch);
 route.post('/login', UsersValidations.loginValidation, UsersControllers.login);
-route.get('/clubs', ClubsControllers.getAll);
-route.get('/matchs', MatchsControllers.getAll);
-route.post('/matchs', MatchsControllers.create);
-route.get('/leaderboard/home', LeaderboardsControllers.getAll);
-route.get('/leaderboard/away', LeaderboardsControllers.getAll);
-
 route.get('/login/validate', UsersValidations.tokenValidation, UsersControllers.getRole);
 
+route.get('/clubs', ClubsControllers.getAll);
 route.get('/clubs/:id', ClubsControllers.getById);
+
+route.get('/matchs', MatchsControllers.getAll);
+route.post('/matchs', MatchsControllers.create);
+route.patch('/matchs/:id', MatchsControllers.updateMatch);
+route.patch('/matchs/:id/finish', MatchsControllers.finishMatch);
+
+route.get('/leaderboard', LeaderboardsControllers.getAll);
+route.get('/leaderboard/home', LeaderboardsControllers.getAll);
+route.get('/leaderboard/away', LeaderboardsControllers.getAll);
 
 export default route;
